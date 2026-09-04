fdff7b72296ca0538e59c588430d18800cbe0164
HANSONGYI <hhan1218@naver.com>
2026-09-04 12:11:47 +0900

    Feat: swc 재설치 2

--- command-service/next.config.js
+++ command-service/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: "standalone",
+
+  experimental: {
+    outputFileTracingIncludes: {
+      "/*": ["./node_modules/@swc/helpers/**/*"],
+    },
+  },
 };
 
 module.exports = nextConfig;
