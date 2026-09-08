1a5e8adfb9aee4d4f337d05900131d7c457a7559
HANSONGYI <hhan1218@naver.com>
2026-09-08 14:52:20 +0900

    Feat: build test

--- command-service/src/app/page.ts
+++ command-service/src/app/page.ts
@@ -2,7 +2,7 @@ import { db } from "../prisma/db";
 
 async function main() {
   await db.$queryRaw`SELECT 1`;
-  console.log("MariaDB 연결 성공");
+  console.log("MariaDB 연결 성공!");
   await db.$disconnect();
 }
 
